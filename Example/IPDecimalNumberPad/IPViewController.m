253528287df164eb095edb33e4a74f3b5de35cc1
Nicholas Servidio <nick@intrepid.io>
2015-10-26 07:29:40 -0400

    added more to the example project

--- Example/IPDecimalNumberPad/IPViewController.m
+++ Example/IPDecimalNumberPad/IPViewController.m
@@ -7,23 +7,47 @@
 //
 
 #import "IPViewController.h"
+#import "IPDecimalNumberPad.h"
 
-@interface IPViewController ()
+@interface IPViewController () <IPDecimalNumberPadDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *numberLabel;
+@property (weak, nonatomic) IBOutlet IPDecimalNumberPad *numberPad;
 
 @end
 
 @implementation IPViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.numberPad.delegate = self;
+    self.numberLabel.text = @"";
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)decimalNumberPadDidPressDeleteButton:(IPDecimalNumberPad *)decimalNumberPad {
+    [self removeCharacterFromNumberLabel];
+}
+
+- (void)decimalNumberPadDidPressDecimalPointButton:(IPDecimalNumberPad *)decimalNumberPad {
+    [self addCharacterToNumberLabel:@"."];
+}
+
+- (void)decimalNumberPad:(IPDecimalNumberPad *)decimalNumberPad didSelectValue:(NSUInteger)value {
+    [self addCharacterToNumberLabel:[NSString stringWithFormat:@"%ld", value]];
+}
+
+- (void)removeCharacterFromNumberLabel {
+    NSString *numberText = self.numberLabel.text;
+    if (numberText.length > 0) {
+        numberText = [numberText substringToIndex:numberText.length - 1];
+        self.numberLabel.text = numberText;
+    }
+}
+
+- (void)addCharacterToNumberLabel:(NSString *)character {
+    NSMutableString *mutableNumberText = [self.numberLabel.text mutableCopy];
+    [mutableNumberText appendString:character];
+    self.numberLabel.text = [mutableNumberText copy];
 }
 
 @end
